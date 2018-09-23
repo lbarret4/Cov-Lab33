37ea25e3e231a4b2f4f9a1f7f26973bc9643f7dd
lbarret4 <llewellynbar4@gmail.com>
2018-09-23 18:56:03 -0400

    removed goodbye import

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Home from './Home';
-import GoodbyeWorld from './goodbye';
 import Navbar from './Navbar';
 
 
