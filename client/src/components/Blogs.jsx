0d423a75510b055d5395e86d60f53cb6c4b613af
lbarret4 <llewellynbar4@gmail.com>
2018-09-24 19:25:39 -0400

    abstacted single blog out into separate component

--- client/src/components/Blogs.jsx
+++ client/src/components/Blogs.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
-import { render } from 'react-dom';
+import Blog from './Blog';
+
 
 class Blogs extends Component {
 
@@ -7,13 +8,7 @@ class Blogs extends Component {
     render() {
         let blogs = [1, 2, 3, 4, 5, 6].map(() => {
             return (
-                <div className="col-md-4">
-                    <h2>Blog</h2>
-
-                    <p>Words Words Words .Words .Words .Words. Words. Words .Words Words Words Words .Words .Words .Words. Words. Words .Words Words Words Words .Words .Words .Words. Words. Words .Words Words Words Words .Words .Words .Words. Words. Words .Words Words Words Words .Words .Words .Words. Words. Words .Words Words Words Words .Words .Words .Words. Words. Words <span className="d-inline-block text-truncate" style={{maxWidth:"150px"}}>Words Words Words Words .Words .Words .Words. Words. Words .Words Words Words Words .Words .Words .Words. Words. Words .Words Words Words Words .Words .Words .Words. Words. Words .Words Words Words Words .Words .Words .Words. Words. Words .Words Words Words Words .Words .Words .Words. Words. Words .Words Words Words Words .Words .Words .Words. Words. Words .Words</span></p>
-
-                    <p><a className="btn btn-secondary" href="#" role="button">View entry &raquo;</a></p>
-                </div>
+             <Blog />
             );
         })
         return (
