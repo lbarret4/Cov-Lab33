69877f7f077e6d9c6e6f63aec5f95123944ab201
lbarret4 <llewellynbar4@gmail.com>
2018-09-24 19:26:42 -0400

    Change class of featured blog and added single blog component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { render } from 'react-dom';
 import BlogFeed from './BlogFeed'
+import Blog from './Blog';
 class Home extends Component {
 
     constructor(props) {
@@ -11,14 +12,10 @@ class Home extends Component {
         return (
             <Fragment>
                 <main role="main">
-                    <div className="jumbotron">
-                        <div className="container">
-                            <h1 className="display-3">Hello, world!</h1>
-                            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-                            <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+                    <div className="jumbotron jumbotron-fluid my-1">
+                        <div className="container d-flex flex-row">           
+                            <Blog type='featured' />
                         </div>
-
-
                     </div>
                     <BlogFeed />
 
