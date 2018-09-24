a704751f991ad2539a11d936c50f7898b42d1a5b
lbarret4 <llewellynbar4@gmail.com>
2018-09-24 19:22:19 -0400

    Changed navabar component type so active tab can change once clicked

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,44 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = (props) => {
-    return (
-        <nav className=" bg-light" >
-            {/* <div classNames="collapse navbar-collapse" id="navbarNavDropdown">
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            home: 'active',
+            profile: '',
+            contact: ''
+        };
+
+        this.handlesOnClick = this.handlesOnClick.bind(this);
+    }
+
+    handlesOnClick(tab, e) {
+        
+        for (let state in this.state){
+            
+            if(this.state[state] === 'active'){
+                this.setState({
+                    [state]:' '
+                });
+
+            }else if(state === tab){
+                this.setState({
+                    [state]:'active'
+                });
+
+            }
+        }
+      
+       
+
+    }
+
+    render() {
+        return (
+
+            <nav className=" bg-light" >
+                {/* <div classNames="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul className="navbar-nav">
                     <li className="nav-item">
                         <Link className="nav-link" to="/">Go Home</Link>
@@ -12,23 +46,24 @@ const Navbar = (props) => {
 
                 </ul>
             </div> */}
-            {/* props.match.pral */}
-            <ul className="nav nav-tabs">
-                <li className="nav-item">
-                <Link className="nav-link active" to="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                <Link className="nav-link " to="/profile">Profile</Link>
-                </li>
-                <li className="nav-item">
-                <Link className="nav-link" to="/contact">Contact</Link>
-                </li>
+                {/* props.match.pral */}
+                <ul className="nav nav-tabs">
+                    <li className="nav-item">
+                        <Link className={`nav-link ${this.state.home}`} to="/" onClick={this.handlesOnClick.bind(this,'home')}>Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className={`nav-link ${this.state.profile}`} to="/profile" onClick={this.handlesOnClick.bind(this,'profile')}> Profile</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className={`nav-link ${this.state.contact}`} to="/contact" onClick={this.handlesOnClick.bind(this,'contact')}>Contact</Link>
+                    </li>
 
 
-            </ul>
-        </nav>
+                </ul>
+            </nav >
 
-    );
+        );
+    }
 }
 
 
