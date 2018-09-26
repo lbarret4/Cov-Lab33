efd0c5a6be10a21240b80c771d0133136150039d
lbarret4 <llewellynbar4@gmail.com>
2018-09-26 11:15:19 -0400

    added tags to edit blog compenent

--- client/src/components/BlogEdit.jsx
+++ client/src/components/BlogEdit.jsx
@@ -1,13 +1,45 @@
 
 import React, { Component, Fragment } from 'react';
+import Navbar from './Navbar';
 
 
 
 class BlogEdit extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tags: []
+        }
+    }
+
+    async componentDidMount() {
+        let url = `http://localhost:3000/api/tags`;
+        try {
+            let results = await fetch(url);
+            let data = await results.json();
+
+            this.setState({
+                tags: await data
+            })
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
 
     render() {
+        let tags = this.state.tags.map((tag, index) => {
+            let colorClass = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark'];
+            let color = colorClass[Math.ceil(Math.random() * colorClass.length) - 1];
+            return (<div className="col-3">
+                <span className={`badge badge-${color}`} key={tag['_created']}>{tag.name}</span>
+            </div>
+            );
+        })
         return (<Fragment>
-            <div className="modal-content">
+            <Navbar tab='post' />
+            <div className="modal-content my-1">
                 <div className="modal-header">
                     <h5 className="modal-title">Post a New Blog Entry</h5>
                     <button type="button" className="close" >
@@ -24,16 +56,19 @@ class BlogEdit extends Component {
 
                         </div>
                     </form>
+                    <div className="row d-flex flex-wrap">
+                        <span className="d-block col-12"> Tags:</span> {tags}
+                    </div>
                     <div className="modal-footer">
 
                         <button type="button" className="btn btn-primary" >Save changes</button>
                     </div>
                 </div>
-                </div>
+            </div>
         </Fragment>
-            );
-            
-        }
+        );
+
     }
-    
+}
+
 export default BlogEdit;
