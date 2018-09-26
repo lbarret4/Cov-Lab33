742159fa43978c0fa508724d74dc3351a4cc0b02
lbarret4 <llewellynbar4@gmail.com>
2018-09-26 11:12:56 -0400

    fixed static title and added router link to navigate to single blog

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-
+import { Link } from 'react-router-dom';
 class Blog extends Component {
 
     constructor(props) {
@@ -8,32 +8,37 @@ class Blog extends Component {
 
 
 
+
+
+
+
     render() {
         let title;
         let truncate = "";
         let blog = this.props.blog;
         let divClass;
+        let path = `/blogs/${blog.id}/entry`;
         if (this.props.type === 'featured') {
-            title= <h1 className="display-3">{blog.title}</h1>;
-            divClass="col";  
-            truncate="";
-        } else {           
-            title=<h2>Blog Title</h2>;
-            divClass="col-md-4 d-flex justify-content-center flex-column";
-            // truncate="text-truncate";
+            title = <h1 className="display-3">{blog.title}</h1>;
+            divClass = "col";
+            truncate = "";
+        } else {
+            title = <h2>{blog.title}</h2>;
+            divClass = "col-md-4 d-flex justify-content-center flex-column";
+            truncate = "text-truncate";
         }
 
         return (
             <Fragment>
 
-            <div className={divClass}>
-               {title}
-                <p className={truncate}> {blog.content}</p>
-                <small className="text-muted d-flex justify-content-end">{blog.date.toLocaleDateString()}</small>
-                <p><a className="btn text-white blogBtn" href="#" role="button">Read more&raquo;</a></p>
-                 
-            </div>
-        </Fragment>
+                <div className={divClass}>
+                    {title}
+                    <p className={truncate}> {blog.content}</p>
+                    <small className="text-muted d-flex justify-content-end">{blog.date.toLocaleDateString()}</small>
+                    <p><Link className="btn text-white blogBtn" to={path} /* onClick={this.handlesOnClick.bind(this, st)} */>Read more &raquo;</Link></p>
+
+                </div>
+            </Fragment>
         );
 
     }
