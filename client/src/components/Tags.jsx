696f623a248b2cecd16a7b62b0abbd594ca34221
lbarret4 <llewellynbar4@gmail.com>
2018-09-27 13:23:10 -0400

    added checkbox value to tags array

--- client/src/components/Tags.jsx
+++ client/src/components/Tags.jsx
@@ -7,12 +7,13 @@ class Tags extends Component {
         this.state = {
             tags: [],
             isVisible: false,
-            otherTag:'',
-            otherVaule:''
+            otherTag: '',
+            otherValue: ''
+
         }
         this.handlesOnChange = this.handlesOnChange.bind(this);
-        this.handlesNewTag =this.handlesNewTag.bind(this);
-        this.handlesOnKeyDown =this.handlesOnKeyDown.bind(this);
+        this.handlesNewTag = this.handlesNewTag.bind(this);
+        this.handlesOnKeyDown = this.handlesOnKeyDown.bind(this);
     }
 
     async componentDidMount() {
@@ -20,7 +21,13 @@ class Tags extends Component {
         try {
             let results = await fetch(url);
             let data = await results.json();
-            data.push({ name: 'other', '_created':`${new Date(Date.now())}` })
+            data.push({ name: 'other', '_created': `${new Date(Date.now())}` });
+            data = data.map((tag) => {
+                if (tag.name !== 'other') {
+                    tag.checked = false;
+                }
+                return tag;
+            });
             this.setState({
                 tags: await data
             })
@@ -30,64 +37,87 @@ class Tags extends Component {
 
     }
     handlesOnChange(e) {
-        if (e.target.checked) {
+        let target = e.target;
+        if (target.checked && target.name == 'other') {
+            let newTags = this.state.tags;
+            let other = newTags.pop();
+            other.checked = !other.checked;
+            newTags.push(other);
             this.setState({
-                isVisible: true
+                isVisible: true,
+                tags: newTags
             });
 
-        } else {
+        } else if (target.name === 'other') {
             this.setState({
                 isVisible: false
             });
+
+
+        } else {
+            this.state.tags.forEach((tag, index) => {
+                if (tag.name === e.target.name) {
+                    let newTags = this.state.tags;
+                    newTags[index].checked = !newTags[index].checked;
+                    this.setState({
+                        tags: newTags
+
+                    });
+                }
+
+            });
+
+
         }
 
     }
 
-    handlesNewTag(e){
+    handlesNewTag(e) {
         this.setState({
-            otherTag:e.target.value
+            otherTag: e.target.value
         });
 
     }
 
-    handlesOnKeyDown(e){
-       if(e.key==='Enter'  && this.state.otherTag.length>0){
-        let url ='http://localhost:3000/api/tags';
-        let tag={};
-        tag.name=this.state.otherTag;
-        tag["_created"]=new Date(Date.now());
-        tag=JSON.parse(JSON.stringify(tag));
-        tag["_created"]=tag["_created"].slice(0, -1);
-        let options ={
-            method:'Post',
-            body:JSON.stringify(tag),
-            headers:{
-                'Content-Type':'application/json'
-            },
-        };
-        (async() =>{
-            try {
-                let results = await fetch(url,options);
-                results= await results.json();
-                tag.id= await results.id;
-                let newTags= this.state.tags;
-                let other = newTags.pop();
-                newTags.push(tag,other);
-                this.setState({
-                    tags:newTags,
-                    otherTag:' ',
-                    isVisible:false,
-                    otherVaule:' '
-                });
-                
-            } catch (error) {
-                console.log(error);
-            }
-        })();
+    handlesOnKeyDown(e) {
+        if (e.key === 'Enter' && this.state.otherTag.length > 0) {
+            let url = 'http://localhost:3000/api/tags';
+            let tag = {};
+            tag.name = this.state.otherTag;
+            tag["_created"] = new Date(Date.now());
+            tag = JSON.parse(JSON.stringify(tag));
+            tag["_created"] = tag["_created"].slice(0, -1);
+            let options = {
+                method: 'Post',
+                body: JSON.stringify(tag),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            };
+            (async () => {
+                try {
+                    let results = await fetch(url, options);
+                    results = await results.json();
+                    tag.id = await results.id;
+                    tag.checked = true;
+                    let newTags = this.state.tags;
+                    let other = newTags.pop();
+                    newTags.push(tag, other);
+                    this.setState({
+                        tags: newTags,
+                        otherTag: ' ',
+                        isVisible: false,
+                        otherValue: ' '
+                    });
 
-       }
+                } catch (error) {
+                    console.log(error);
+                }
+            })();
+
+        }
     }
-    
+
 
     render() {
         let tags = this.state.tags.map((tag, index) => {
@@ -96,13 +126,13 @@ class Tags extends Component {
             let checkbox;
             if (tag.name === 'other') {
                 checkbox = (<Fragment>
-                    <input type="checkbox" className="custom-control-input" id={`customCheck${index}`} key={index + tag['_created']} value={this.state.otherVaule} onChange={this.handlesOnChange} v />
+                    <input type="checkbox" className="custom-control-input" name={tag.name} id={`customCheck${index}`} key={index + tag['_created']} value={this.state.otherValue} onChange={this.handlesOnChange} />
                     {label}
-                    <input className={`form-control form-control-sm ${visibility}`} type="text" onKeyDown={this.handlesOnKeyDown} onChange={this.handlesNewTag} value={this.state.otherTag}/>
+                    <input className={`form-control form-control-sm ${visibility}`} type="text" onKeyDown={this.handlesOnKeyDown} onChange={this.handlesNewTag} value={this.state.otherTag} />
                 </Fragment>);
             } else {
                 checkbox = (<Fragment>
-                    <input type="checkbox" className="custom-control-input" id={`customCheck${index}`} key={index + tag['_created']} />
+                    <input type="checkbox" className="custom-control-input" name={tag.name} checked={this.state.tags[index].checked} id={`customCheck${index}`} onChange={this.handlesOnChange} key={index + tag['_created']} />
                     {label}
                 </Fragment>);
             }
