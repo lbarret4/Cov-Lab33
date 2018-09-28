ee7281c2b943be858cc719067ce6e6be313eeb8a
lbarret4 <llewellynbar4@gmail.com>
2018-09-28 02:27:31 -0400

    added ability to display tags for a blog entry

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -5,7 +5,8 @@ class BlogCard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            blog: '',
+            blog: {},
+            tags: []
         }
     }
 
@@ -13,16 +14,18 @@ class BlogCard extends Component {
     async componentDidMount() {
         let path = this.props.match.url;
         if (path.includes('blogs')) {
+            let id = this.props.match.params.id;
             let index = path.indexOf('entry') - 1;
-            let url = `http://localhost:3000/api/${path.slice(0, index)}`;
+            let url1 = `http://localhost:3000/api/${path.slice(0, index)}`;
+            let url2 = `http://localhost:3000/api/blogs/blogtags/${id}`;
             try {
-                let results = await fetch(url);
-                let data = await results.json();
-                data.date = new Date(await data['_created']);
-                delete data["_created"];
-                console.log(data.date.toLocaleDateString());
+                let [res1, res2] = await Promise.all([fetch(url1), fetch(url2)]);
+                let [data1, data2] = await Promise.all([res1.json(), res2.json()]);
+                data1.date = new Date(await data1['_created']);
+                delete data1["_created"];
                 this.setState({
-                    blog: await data,
+                    blog: await data1,
+                    tags: await data2
                 });
             } catch (error) {
                 console.log(error);
@@ -36,13 +39,28 @@ class BlogCard extends Component {
     render() {
         let blog = this.state.blog;
         let tabTitle = this.props.match.url.split("/").slice(-1);
+        let label;
+        let tags = this.state.tags.map((tag, index) => {
+            if(this.state.tags.length !== 0){
+                return (
+            <label className={`badgeMod badge-info `} key={tag['_created'] + index}>{tag.name}</label>);
+             }else{
+                 return;
+             }
+        });
         return (
             <Fragment>
                 <Navbar tab={tabTitle} path={this.props.match.url} />
                 <div className="card my-1 ">
                     <div className="card-header text-center" >{blog.title}</div>
-                    <div className="card-body">{blog.content} </div>
-                    <div className="card-footer text-muted">{blog.date ? blog.date.toLocaleDateString() : blog}</div>
+                    <div className="card-body">
+                        <span className="text-right  text-muted d-block">{blog.date ? blog.date.toLocaleDateString() : ''}</span>
+                        {blog.content}
+                    </div>
+                    <div className="card-footer form-inline justify-content-around">
+                    {tags.length !== 0? <span className="d-block col-12"> Tags:</span>: ''}
+                        {tags}
+                    </div>
                 </div>
             </Fragment>
         );
